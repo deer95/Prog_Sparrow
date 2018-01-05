141177d5576ed75f0aa34486c8288b182b47f0e9
deer95@yandex.ru <deer95@yandex.ru>
2018-01-06 01:05:39 +0300

    edited file "8.Backwards_Dictionary.py" and deleted file "1.py"

--- 8.Backwards_Dictionary.py
+++ 8.Backwards_Dictionary.py
@@ -1,15 +1,22 @@
 import re
 
 def sorter(word):
-    return [hierarchy.index(ord(word[i])) for i in range(len(word))]
+    return word[::-1]
 
-text = 'Qatna (modern: Arabic, Tell al-Mishrifeh) is an ancient city located in Homs Governorate, Syria. Its remains constitute a tell situated about 18 km (11 mi) northeast of Homs near the village of al-Mishrifeh. The city was an important center throughout most of the second millennium BC and in the first half of the first millennium BC. It contained one of the largest royal palaces of Bronze Age Syria and an intact royal tomb that provided a great amount of data on the funerary habits of that period.'.lower()
-text = re.sub('[!@$%\^&\*\(\)\?><,\.\":;1234567890]', '', text)
-text_array = set(text.split())
-#list(text_array).sort(key = sorter)
-#for elt in text_array:
-#    print(elt)
 
-text_array = sorted(list(text_array))
-for i in range(len(text_array) - 1, -1, -1):
-    print(text_array[i])
+text = open('8.Backwards_Dictionary_text.txt', 'r', encoding = 'utf-8')
+text_array = set()
+
+for line in text:
+    line = line.split()
+    for word in line:
+        text_array.add(re.sub('[^\w-]', '', word))
+
+text_array = list(text_array)
+text_array.sort(key=sorter)
+
+for elt in text_array:
+    print(elt)
+
+
+text.close()
