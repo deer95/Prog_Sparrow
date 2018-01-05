36db276a57741e1b353a283b6f39f396820cd5a5
deer95@yandex.ru <deer95@yandex.ru>
2018-01-06 01:57:49 +0300

    edited file "7.18 Deleting X doubles"

--- 7.18 Deleting X doubles.py
+++ 7.18 Deleting X doubles.py
@@ -5,28 +5,22 @@ l = [1, 2, 1, 2, 3, 4, 5, 6] * n
 threshold = n
 
 t = time.time()
-
 i = 0
+
 while i < len(l):
     elt = l[i]
     num_elt = l.count(elt)
-
-
-    j = i + 1
-    while num_elt == threshold:
-        if elt == l[j]:
-            del l[j]
-            num_elt -= 1
-        else:
-            j += 1
-
-'''
     if num_elt == threshold:
-        for j in range(num_elt):
-            l.remove(elt)
+        j = 0
+
+        while j < len(l):
+            if l[i] == l[j]:
+                del l[j]
+            else:
+                j += 1
+
     else:
         i += 1
-'''
 
 t = time.time() - t
 print(t)
