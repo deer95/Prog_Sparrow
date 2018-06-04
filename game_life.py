c284b76dc28fd2fe55814d50e6657a2bb447ac53
deer95@yandex.ru <deer95@yandex.ru>
2018-06-05 02:43:46 +0300

    edited game_life.py

--- game_life.py
+++ game_life.py
@@ -1,9 +1,12 @@
 import random
+import os
+import time
 
 
 def enter_data():
     '''ASKING USER TO ENTER DATA'''
     # enter square
+    global length, height
     while True:
         try:
             length, height = [int(i) for i in input('Enter the size in format like "100 100": ').split()]
@@ -28,12 +31,11 @@ def enter_data():
                 print('Sorry, the number is inappropriate. Try something between 0 and {}.'.format(square))
             else:
                 break
-    return (length, height, num_cells)
+    return num_cells
 
 
-def generate_cells(tuple):
+def generate_cells(num_cells):
     '''ESTABLISHING THE POSITION OF ALIVE CELLS'''
-    length, height, num_cells = tuple
     cells_set = set()
     for i in range(num_cells):
         row = random.randint(0, height - 1)
@@ -44,34 +46,120 @@ def generate_cells(tuple):
             column = random.randint(0, length - 1)
             coordinate = (row, column)
         cells_set.add(coordinate)
-    return (cells_set, length, height)
+    return cells_set
 
 
-def print_field(tuple):
+def find_neighbours(row, column):
+    '''FIND NEIGHBOURS FOR A CELL'''
+    cell_neighbours = set()
+    diff_row,  diff_column = -1, -1
+    while diff_row <= 1:
+        while diff_column <= 1:
+            new_row = row + diff_row
+            new_column = column + diff_column
+
+            # excluding the cell itself
+            if new_row == row and new_column == column:
+                diff_column += 1
+                continue
+
+            # checking the borders
+            if max(new_row, new_column) < 0 or new_row >= height or new_column >= length:
+                diff_column += 1
+                continue
+
+            bour = (new_row, new_column)
+            cell_neighbours.add(bour)
+
+            diff_column += 1
+        diff_row += 1
+        diff_column = -1
+    return cell_neighbours
+
+
+def produce_total_neighbours(cells_set):
+    '''MAKING A SET OF ALL CELLS THAT CAN CHANGE'''
+    neighbours = set()
+    for row, column in cells_set:
+        # find all possible neighbours
+        cell_neighbours = find_neighbours(row, column)
+        neighbours |= cell_neighbours
+    return neighbours
+
+
+def animate(alive_cells, neighbours):
+    '''BREATHING LIFE'''
+    animated_set = set()
+    for cell in neighbours:
+        cell_neighbours = find_neighbours(cell[0], cell[1])
+        if len(alive_cells & cell_neighbours) == 3:
+            animated_set.add(cell)
+    return animated_set
+
+
+def kill(alive_cells):
+    '''KILLING INNOCENT'''
+    dead_set = set()
+    for cell in alive_cells:
+        cell_neighbours = find_neighbours(cell[0], cell[1])
+        if len(alive_cells & cell_neighbours) > 3 or len(alive_cells & cell_neighbours) < 2:
+            dead_set.add(cell)
+    return dead_set
+
+
+def changing_field(alive_cells, neighbours):
+    '''PERFORMING THE LIFE PROCESS'''
+    animated_set = animate(alive_cells, neighbours)
+    new_cells_set = alive_cells | animated_set
+    dead_set = kill(cells_set)
+    # continue if there are changes, break if not
+    if animated_set or dead_set:
+        new_cells_set -= dead_set
+        return new_cells_set
+    else:
+        return False
+
+
+
+def print_field(alive_cells):
     '''PRINTING THE FINAL FIELD'''
     # making upper borders
-    alive_cells, length, height = tuple
     print('┌', end='')
-    for i in range(length):
+    for i in range(length + 2):
         print('-', end='')
     print('┐')
 
     # printing the field
     for i in range(height):
-        print('│', end='')
+        print('│ ', end='')
         for j in range(length):
             if (i, j) in alive_cells:
                 print('+', end='')
             else:
                 print(' ', end='')
-        print('│')
+        print(' │')
 
     # making lower borders
     print('└', end='')
-    for i in range(length):
+    for i in range(length + 2):
         print('-', end='')
     print('┘')
 
 
-while input('Finish?') == '':
-    print_field(generate_cells(enter_data()))
+length, height = 0, 0
+
+cells_set = generate_cells(enter_data())
+print_field(cells_set)
+while cells_set:
+    total_neighbours = produce_total_neighbours(cells_set)
+    new_cells_set = changing_field(cells_set, total_neighbours)
+    if new_cells_set == cells_set:
+        break
+    else:
+        cells_set = new_cells_set
+        time.sleep(0.5)
+        print("\n" * 5)
+        #os.system('cls')
+        print_field(cells_set)
+
+print('THE GAME IS OVER. LONG LIVE THE LIFE!')
